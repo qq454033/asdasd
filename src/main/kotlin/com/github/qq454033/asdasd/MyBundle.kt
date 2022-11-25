aac30805dc0b2b0a19be255d645a7c2dec674c58
GitHub Action <action@github.com>
2022-11-25 15:50:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/qq454033/asdasd/MyBundle.kt
+++ src/main/kotlin/com/github/qq454033/asdasd/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.qq454033.asdasd
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/qq454033/asdasd/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qq454033/asdasd/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.qq454033.asdasd.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qq454033.asdasd.MyBundle
 
 class MyProjectService(project: Project) {
 
